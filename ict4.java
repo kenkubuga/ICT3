7ff5b0380083fef4a5839f0888765474ac53b1f6
kenkubuga <126837475+kenkubuga@users.noreply.github.com>
2023-03-03 14:28:21 +0000

    Ict 4

---
+++ ict4.java
@@ -0,0 +1,12 @@
+
+public class ict3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10);
+		System.out.println(10+1);
+		System.out.println("ten");
+		
+	}
+
+}
